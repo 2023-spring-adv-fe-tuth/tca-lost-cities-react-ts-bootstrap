f17437d35f2f541919077a4982f6fd619ad949bb
emjohnson3 <emjohnson3@madisoncollege.edu>
2023-05-05 13:01:40 -0500

    Remove buttons because it's a terrible idea

--- src/Play.tsx
+++ src/Play.tsx
@@ -66,50 +66,7 @@ export const Play: React.FC<PlayProps> = ({
                 <Card.Header>
                     Wagers
                 </Card.Header>
-                <Card.Body> 
-                    <p>Player One, Round One: Number of Wagers</p>            
-                    <input type="radio" name="p1r1wager" value="0" id="p1r1_w0"/>
-                    <label htmlFor="p1r1_w0">0</label> &nbsp;      
-                    <input type="radio" name="p1r1wager" value="1" id="p1r1_w1" />
-                    <label htmlFor="p1r1_w1">1</label> &nbsp;
-                    <input type="radio" name="p1r1wager" value="2" id="p1r1_w2" />
-                    <label htmlFor="p1r1_w2">2</label> &nbsp;
-                    <input type="radio" name="p1r1wager" value="3" id="p1r1_w3" />
-                    <label htmlFor="p1r1_w3">3</label> &nbsp;
-                    <input type="radio" name="p1r1wager" value="4" id="p1r1_w4" />
-                    <label htmlFor="p1r1_w4">4</label> &nbsp;
-                    <input type="radio" name="p1r1wager" value="5" id="p1r1_w5" />
-                    <label htmlFor="p1r1_w5">5</label> 
-                </Card.Body>
-                <Card.Body> 
-                    <p>Player One, Round One: Successful Wagers</p>            
-                    <input type="radio" name="p1r1success" value="0" id="p1r1_s0"/>
-                    <label htmlFor="p1r1_s0">0</label> &nbsp; 
-                    <input type="radio" name="p1r1success" value="1" id="p1r1_s1"/>
-                    <label htmlFor="p1r1_s1">1</label> &nbsp; 
-                    <input type="radio" name="p1r1success" value="2" id="p1r1_s2"/>
-                    <label htmlFor="p1r1_s2">2</label> &nbsp; 
-                    <input type="radio" name="p1r1success" value="3" id="p1r1_s3"/>
-                    <label htmlFor="p1r1_s3">3</label> &nbsp; 
-                    <input type="radio" name="p1r1success" value="4" id="p1r1_s4"/>
-                    <label htmlFor="p1r1_s4">4</label> &nbsp; 
-                    <input type="radio" name="p1r1success" value="5" id="p1r1_s5"/>
-                    <label htmlFor="p1r1_s5">5</label> &nbsp; 
-                </Card.Body>
-                <Card.Body> 
-                    <p>Player One, Round Two: Number of Wagers</p>            
-                    <input type="radio" name="p1r2wager" value="0" id="p1r2_w0"/>
-                    <label htmlFor="p1r2_w0">0</label> &nbsp; 
-                    <input type="radio" name="p1r2wager" value="1" id="p1r2_w1"/>
-                    <label htmlFor="p1r2_w1">1</label> &nbsp; 
-                    <input type="radio" name="p1r2wager" value="2" id="p1r2_w2"/>
-                    <label htmlFor="p1r2_w2">2</label> &nbsp; 
-                    <input type="radio" name="p1r2wager" value="3" id="p1r2_w3"/>
-                    <label htmlFor="p1r2_w3">3</label> &nbsp; 
-                    <input type="radio" name="p1r2wager" value="4" id="p1r2_w4"/>
-                    <label htmlFor="p1r2_w4">4</label> &nbsp; 
-                    <input type="radio" name="p1r2wager" value="5" id="p1r2_w5"/>
-                    <label htmlFor="p1r2_w5">5</label> &nbsp; 
+                <Card.Body>                     
                 </Card.Body>
             </Card>
         </>          
