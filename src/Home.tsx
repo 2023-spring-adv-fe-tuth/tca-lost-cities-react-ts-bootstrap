1427ac801a0ecb1da08d5de494e298cd617810ff
emjohnson3 <emjohnson3@madisoncollege.edu>
2023-05-08 09:31:51 -0500

    5/8 meeting start

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,6 +6,9 @@ import { LeaderboardPlayer } from './front-end-model';
 import Table from 'react-bootstrap/Table';
 import { durationFormatter } from 'human-readable'; 
 import CardHeader from 'react-bootstrap/esm/CardHeader';
+import React, { useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { CDBContainer } from 'cdbreact';
 
 interface HomeProps {
     leaderboardData: LeaderboardPlayer[];
@@ -35,7 +38,7 @@ export const Home: React.FC<HomeProps> = ({
 
     const nav = useNavigate();
 
-    const format = durationFormatter();
+    const format = durationFormatter();    
 
     return (
         <>
@@ -127,6 +130,45 @@ export const Home: React.FC<HomeProps> = ({
                     </p>                    
                 </Card.Body>
             </Card>
+            <Card>
+                <CardHeader>
+                    Bar Chart Default
+                </CardHeader>
+                <Card.Body>
+                    <p>
+                        {`Happens ${(reallyCoolThingHappenedPercent * 100).toFixed(2)}% of games`}
+                    </p>                    
+                </Card.Body>
+            </Card>
         </>
     )
 };
+
+const Chart = () => {
+    const [data] = useState({
+      labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: 'rgba(194, 116, 161, 0.5)',
+          borderColor: 'rgb(194, 116, 161)',
+          data: [65, 59, 90, 81, 56, 55, 40],
+        },
+        {
+          label: 'My Second dataset',
+          backgroundColor: 'rgba(71, 225, 167, 0.5)',
+          borderColor: 'rgb(71, 225, 167)',
+          data: [28, 48, 40, 19, 96, 27, 100],
+        },
+      ],
+    });
+  
+    return (
+      <CDBContainer>
+        <h3 className="mt-5">Bar chart</h3>
+        <Bar data={data} options={{ responsive: true }} />
+      </CDBContainer>
+    );
+};
+
+export default Chart;
